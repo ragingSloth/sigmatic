b93f588be6919b273fbb852c72f5dd829baec0a2
ezra <ezra@indico.io>
2015-01-05 18:17:39 -0500

    we have parse

--- lisp.rs
+++ lisp.rs
@@ -6,6 +6,7 @@
 
 extern crate rustc;
 extern crate syntax;
+extern crate alloc;
 use syntax::codemap::Span;
 use syntax::parse::token;
 use syntax::ast::{TokenTree, TtToken, TtDelimited, Delimited};
@@ -18,42 +19,71 @@ use rustc::plugin::Registry;
 use std::os;
 use std::num::{Num};
 use std::io::File;
+use alloc::rc::{Rc};
 use Tree::*;
 
 
-pub trait Lisp
-{
-    fn parse<'a>(&'a mut self) -> Tree<'a>;
-}
+//pub trait Lisp
+//{
+//    fn parse<'a>(&'a mut self) -> Tree<'a>;
+//}
+//
+//impl Lisp for TokenTree{
+//    fn parse<'a>(&'a mut self) -> Tree<'a>{ 
+//        match *(self as *mut TokenTree){
+//            TtDelimited(_, y) => {
+//                match y.delim{
+//                    token::DelimToken::Paren => return y.parse(),
+//                    _ => panic!("not done yet"),
+//                }
+//            },
+//            TtToken(_, ref mut t) => return E(box t.clone()),
+//            _ => panic!("not done yet"),
+//        }
+//    }
+//}
+//
+//impl Lisp for Delimited{
+//    fn parse<'a>(&'a mut self) -> Tree<'a>{
+//        let mut LL = vec![];
+//        loop{
+//            match self.tts.pop(){
+//                Some(ref mut x) => {
+//                    let temp = x.parse().clone();
+//                    LL.push(temp);
+//                    },
+//                None => break,
+//            };
+//        }
+//       return St(LL);
+//    }
+//}
 
-impl Lisp for TokenTree{
-    fn parse<'a>(&'a mut self) -> Tree<'a>{ 
-        match *self{
-            TtDelimited(_, ref mut y) => {
-                match y.delim{
-                    token::DelimToken::Paren => return y.parse(),
-                    _ => panic!("not done yet"),
-                }
-            },
-            TtToken(_, ref mut t) => return E(box t.clone()),
-            _ => panic!("not done yet"),
-        }
+fn parse_delimited<'a>(d: Rc<Delimited>) -> Tree<'a>{
+    let mut LL = vec![];
+    let mut ts = d.tts.clone();
+    loop{
+        match ts.pop(){
+            Some(x) => {
+                let temp = parse_TT(x);
+                LL.push(temp);
+                },
+            None => break,
+        };
     }
+   return St(LL);
 }
 
-impl Lisp for Delimited{
-    fn parse<'a>(&'a mut self) -> Tree<'a>{
-        let mut LL = vec![];
-        loop{
-            match self.tts.pop(){
-                Some(ref mut x) => {
-                    let temp = x.parse().clone();
-                    LL.push(temp);
-                    },
-                None => break,
-            };
-        }
-       return St(LL);
+fn parse_TT<'a>(tt: TokenTree) -> Tree<'a>{
+    match tt.clone() {
+        TtDelimited(_, y) => {
+            match y.delim{
+                token::DelimToken::Paren => return parse_delimited(y.clone()),
+                _ => panic!("not done yet"),
+            }
+        },
+        TtToken(_, ref mut t) => return E(box t.clone()),
+        _ => panic!("not done yet"),
     }
 }
 
@@ -68,7 +98,7 @@ fn expand_lisp(cx: &mut ExtCtxt, sp: Span, args: &[TokenTree]) -> Box<MacResult 
     //let mut n = vec![args];
    // println!("{}", n.pop());
     let text = match args[0] {
-        TtDelimited(_, _) => (*args)[0].parse(),
+        TtDelimited(_, ref x) => parse_delimited(x.clone()),
         _ => panic!("that's not lisp"),
     };
     println!("{}", text);
